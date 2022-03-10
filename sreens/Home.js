79ecc32dc744fa0c105941b8e88b5d732b5de02b
mariosknl <mariosknl@outlook.com>
2022-03-10 13:08:47 +0200

    SearchBar component created

--- sreens/Home.js
+++ sreens/Home.js
@@ -1,11 +1,16 @@
 import { View, SafeAreaView, Text } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
+import {} from "@expo/vector-icons";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
     </SafeAreaView>
   );
 }
