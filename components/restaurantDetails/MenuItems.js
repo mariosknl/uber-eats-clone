8e101a422d6118c709af1aff93d1df46e204fb88
mariosknl <mariosknl@outlook.com>
2022-03-10 18:42:17 +0200

    MenuItem created

---
+++ components/restaurantDetails/MenuItems.js
@@ -0,0 +1,55 @@
+import { Image, View, ScrollView, StyleSheet, Text } from "react-native";
+import React from "react";
+
+const foods = [
+  {
+    title: "Tandoori Chicken",
+    description: "Amazing Indian dish with tenderloin chichen off",
+    price: "$19.20",
+    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
+  },
+];
+
+const styles = StyleSheet.create({
+  menuItemStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+  titleStyle: {
+    fontSize: 19,
+    fontWeight: "600",
+  },
+});
+
+export default function MenuItems() {
+  return (
+    <ScrollView showsVerticalScrollIndicatol={false}>
+      {foods.map((food, index) => (
+        <View key={index}>
+          <View style={styles.menuItemStyle}>
+            <FoodInfo food={food} />
+            <FoodImage food={food} />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const FoodInfo = (props) => (
+  <View style={{ width: 240, justifyContent: "space-evenly" }}>
+    <Text style={styles.titleStyle}>{props.food.title}</Text>
+    <Text>{props.food.description}</Text>
+    <Text>{props.food.price}</Text>
+  </View>
+);
+
+const FoodImage = (props) => (
+  <View>
+    <Image
+      source={{ uri: props.food.image }}
+      style={{ width: 100, height: 100, borderRadius: 8 }}
+    />
+  </View>
+);
--- sreens/RestaurantDetail.js
+++ sreens/RestaurantDetail.js
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import About from "../components/restaurantDetails/About";
+import MenuItems from "../components/restaurantDetails/MenuItems";
 
 export default function RestaurantDetail() {
   return (
     <View>
       <About />
+      <MenuItems />
     </View>
   );
 }
